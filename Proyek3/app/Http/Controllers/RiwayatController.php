cefae9f72d6520580859b1bcc22014488e0ff1e7
Viola Insan Putri <violainsanputri@gmail.com>
2026-05-01 18:58:43 +0700

    menambahkan  riwayat

--- Proyek3/app/Http/Controllers/RiwayatController.php
+++ Proyek3/app/Http/Controllers/RiwayatController.php
@@ -15,4 +15,26 @@ class RiwayatController extends Controller
 
         return view('pages.riwayat', compact('pengantarans'));
     }
+public function riwayatKurir($id)
+    {
+        $pengantarans = Pengantaran::with(['kurir', 'pesanan.pelanggan'])
+            ->where('kurir_id', $id)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        $data = $pengantarans->map(function ($item) {
+            return [
+                'nama_penerima' => $item->pesanan->pelanggan->nama ?? '-',
+                'alamat' => $item->pesanan->alamat ?? '-',
+                'status' => $item->status ?? '-',
+                'tanggal' => $item->created_at
+                    ? $item->created_at->format('d M Y')
+                    : '-',
+            ];
+        });
+
+        return response()->json([
+            'data' => $data
+        ]);
+    }
 }
--- Proyek3/routes/api.php
+++ Proyek3/routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\RiwayatController;
 
 Route::post('/login-kurir', [AuthController::class, 'login']);
+Route::get('/riwayat-kurir/{id}', [RiwayatController::class, 'riwayatKurir']);
